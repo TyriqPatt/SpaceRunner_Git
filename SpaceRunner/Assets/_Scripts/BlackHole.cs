1e03d971ec6dc8338b5aab89b58bdb71438292f0
TyriqPatt <38378678+TyriqPatt@users.noreply.github.com>
2020-02-13 02:28:26 -0500

    Straight Fire boss update

--- SpaceRunner/Assets/_Scripts/BlackHole.cs
+++ SpaceRunner/Assets/_Scripts/BlackHole.cs
@@ -4,42 +4,42 @@ using UnityEngine;
 
 public class BlackHole : MonoBehaviour
 {
-    public List<GameObject> Asteroids = new List<GameObject>();
     public GameObject[] Asteriods;
+    public List<GameObject> a = new List<GameObject>();
     public GameObject Asteriod;
 
     // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
-        Asteriods = GameObject.FindGameObjectsWithTag("Asteroid");
+        //Asteriods = GameObject.FindGameObjectsWithTag("Asteroid");
+        
+        //Asteriods = hit[hit.Length].transform.gameObject;
+        //foreach (GameObject s in hit)
+        //{
+
+        //    //s.transform.position -= transform.position;
+        //    s.transform.position = Vector3.MoveTowards(s.transform.position, transform.position, 1);
+        //}
+        //if (hit.transform.tag == "Asteroid")
+        //{
+        //    Asteroids.Add(hit.transform.gameObject);
+        //    hit.transform.parent = transform.parent;
+        //}
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //Asteriod.transform.position = Vector3.MoveTowards(Asteriod.transform.position, transform.position, 1);
-
-        foreach (GameObject s in Asteriods)
-        {
-            //s.transform.position -= transform.position;
-            s.transform.position = Vector3.MoveTowards(s.transform.position, transform.position, 1);
-        }
-
-        RaycastHit hit;
-        if (Physics.SphereCast(transform.position, 100, transform.forward, out hit, 10))
-        {
-            if (hit.transform.tag == "Asteroid")
-            {
-                Asteroids.Add(hit.transform.gameObject);
-                hit.transform.parent = transform.parent;
-            }
-
-        }
-        foreach (GameObject s in Asteroids)
-        {
-            //s.transform.position -= transform.position;
-            s.transform.position = Vector3.MoveTowards(s.transform.position, transform.position, 1);
-        }
+        //foreach (GameObject s in Asteroids)
+        //{
+        //    //s.transform.position -= transform.position;
+        //    s.transform.position = Vector3.MoveTowards(s.transform.position, transform.position, 1);
+        //}
+        RaycastHit[] hit = Physics.SphereCastAll(transform.position, 100, transform.forward, 10);
+        a.Add(hit[hit.Length].transform.gameObject);
     }
 
     private void OnDrawGizmosSelected()
---
+++ SpaceRunner/Assets/_Scripts/PillarFunctionality.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillarFunctionality : MonoBehaviour
+{
+
+    public GameObject[] Sections;
+    Vector3 smoothpos;
+    float Dir = 5;
+    public float speed;
+    public float ExpandSpeed;
+    public float TilNextExpand;
+    float delay;
+    public enum State {Positioning, SetAllActive, Expandp1, Expandp2, Connect1, Connect2, TurnOnLasers, MoveRight, MoveLeft}
+
+    public State PillarState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PillarState = State.SetAllActive;
+        smoothpos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PillarController();
+        transform.position = smoothpos;
+    }
+
+    void PillarController()
+    {
+        switch (PillarState)
+        {
+            case State.Positioning:
+                //Move from Droid boss to V3(0,10,50)
+                break;
+
+            case State.SetAllActive:
+                Sections[1].SetActive(true);
+                Sections[2].SetActive(true);
+                Sections[3].SetActive(true);
+                Sections[4].SetActive(true);
+                PillarState = State.Expandp1;
+                break;
+            case State.Expandp1:
+                if (Sections[1].transform.localPosition.y >= -2 && Sections[2].transform.localPosition.y >= -2)
+                {
+                    Sections[1].transform.localPosition -= transform.right * ExpandSpeed;
+                    Sections[2].transform.localPosition -= transform.right * ExpandSpeed;
+                }
+                if (Sections[3].transform.localPosition.y <= 2 && Sections[4].transform.localPosition.y <= 2)
+                {
+                    Sections[3].transform.localPosition += transform.right * ExpandSpeed;
+                    Sections[4].transform.localPosition += transform.right * ExpandSpeed;
+                }
+                else if(Sections[1].transform.localPosition.y <= -2 && Sections[2].transform.localPosition.y <= -2 && Sections[3].transform.localPosition.y >= 2 && Sections[4].transform.localPosition.y >= 2)
+                {
+                    PillarState = State.Connect1;
+                }
+                break;
+            case State.Connect1:
+                Sections[1].transform.localPosition = new Vector3(0, -2, 0);
+                Sections[2].transform.localPosition = new Vector3(0, -2, 0);
+                Sections[3].transform.localPosition = new Vector3(0, 2, 0);
+                Sections[4].transform.localPosition = new Vector3(0, 2, 0);
+                
+                if(delay < TilNextExpand)
+                {
+                    delay += Time.deltaTime;
+                }
+                if(delay >= TilNextExpand)
+                {
+                    PillarState = State.Expandp2;
+                    delay = 0;
+                }
+                break;
+            case State.Expandp2:
+                if (Sections[2].transform.localPosition.y >= -4)
+                {
+                    Sections[2].transform.localPosition -= transform.right * ExpandSpeed;
+                }
+                if (Sections[4].transform.localPosition.y <= 4)
+                {
+                    Sections[4].transform.localPosition += transform.right * ExpandSpeed;
+                }
+                else if (Sections[2].transform.localPosition.y <= -4 && Sections[4].transform.localPosition.y >= 4)
+                {
+                    PillarState = State.Connect2;
+                }
+                break;
+            case State.Connect2:
+                Sections[2].transform.localPosition = new Vector3(0, -4, 0);
+                Sections[4].transform.localPosition = new Vector3(0, 4, 0);
+                if (delay < TilNextExpand)
+                {
+                    delay += Time.deltaTime;
+                }
+                if (delay >= TilNextExpand)
+                {
+                    StartSpin();
+                    EnableLaser();
+                    PillarState = State.MoveRight;
+                    delay = 0;
+                }
+                break;
+            case State.MoveRight:
+
+
+                if (transform.position.x >= 50)
+                {
+                    PillarState = State.MoveLeft;
+                }
+                else
+                {
+                    smoothpos = Vector3.Lerp(transform.position,
+                        transform.position = new Vector3(transform.position.x + Dir,
+                        transform.position.y, transform.position.z), speed * Time.deltaTime);
+                }
+                break;
+            case State.MoveLeft:
+
+                if (transform.position.x <= -50)
+                {
+                    PillarState = State.MoveRight;
+                }
+                else
+                {
+                    smoothpos = Vector3.Lerp(transform.position,
+                        transform.position = new Vector3(transform.position.x - Dir,
+                        transform.position.y, transform.position.z), speed * Time.deltaTime);
+                }
+                break;
+
+        }
+    }
+
+    void StartSpin()
+    {
+        Sections[0].GetComponent<Rotator>().enabled = true;
+        Sections[1].GetComponent<Rotator>().enabled = true;
+        Sections[2].GetComponent<Rotator>().enabled = true;
+        Sections[3].GetComponent<Rotator>().enabled = true;
+        Sections[4].GetComponent<Rotator>().enabled = true;
+    }
+
+    void EnableLaser()
+    {
+        Sections[0].transform.GetChild(0).GetChild(1).gameObject.SetActive(true);
+        Sections[0].transform.GetChild(1).GetChild(1).gameObject.SetActive(true);
+        Sections[1].transform.GetChild(0).GetChild(1).gameObject.SetActive(true);
+        Sections[1].transform.GetChild(1).GetChild(1).gameObject.SetActive(true);
+        Sections[2].transform.GetChild(0).GetChild(1).gameObject.SetActive(true);
+        Sections[2].transform.GetChild(1).GetChild(1).gameObject.SetActive(true);
+        Sections[3].transform.GetChild(0).GetChild(1).gameObject.SetActive(true);
+        Sections[3].transform.GetChild(1).GetChild(1).gameObject.SetActive(true);
+        Sections[4].transform.GetChild(0).GetChild(1).gameObject.SetActive(true);
+        Sections[4].transform.GetChild(1).GetChild(1).gameObject.SetActive(true);
+        
+    }
+}
