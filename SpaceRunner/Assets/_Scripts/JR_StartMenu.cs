ce42d507f9f12a1856355719f00a191a2826aa8f
DESKTOP-H2UKB99\Jesse Robinson <jbrobinson16@gmail.com>
2020-02-21 00:53:46 -0500

    Added MainMenu with button that leads to level one

---
+++ SpaceRunner/Assets/_Scripts/JR_StartMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JR_StartMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level_1"); 
+    }
+}
