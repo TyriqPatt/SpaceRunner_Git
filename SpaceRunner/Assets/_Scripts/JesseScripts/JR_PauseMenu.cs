1045e1f3429b8f63896e32cc7175316291a73a1d
DESKTOP-H2UKB99\Jesse Robinson <jbrobinson16@gmail.com>
2020-02-22 11:54:54 -0500

    Can pull up pause menu and then remove it with Esc

--- SpaceRunner/Assets/_Scripts/JesseScripts/JR_PauseMenu.cs
+++ SpaceRunner/Assets/_Scripts/JesseScripts/JR_PauseMenu.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class JR_PauseMenu : MonoBehaviour
 {
+    //The pause canvas
+    public GameObject PauseMenu;
+    //Check if it is paused or not
+    private bool isPaused; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,39 @@ public class JR_PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //pause input key
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            //Check if not paused
+            if (!isPaused)
+            {
+                //Pause screen function
+                PauseGame();
+            }
+            //Check if is paused
+            else
+            {
+                //Removes pause screen function
+                UnpauseGame(); 
+            }
+        }
         
+    }
+
+    //Call pause dscreen function
+    private void PauseGame()
+    {
+        //Set the pause canvas true
+        PauseMenu.SetActive(true);
+        //To create the switch on and off feature
+        isPaused = true;
+    }
+    //Remove pause screen
+    private void UnpauseGame()
+    {
+        //Set the pause canvas as false 
+        PauseMenu.SetActive(false);
+        //To create the switch on and off feature
+        isPaused = false;
     }
 }
