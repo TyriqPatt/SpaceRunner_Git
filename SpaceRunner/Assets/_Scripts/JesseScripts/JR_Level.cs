fa7e37a87992268c18fcc6b5623397e7fdaaab83
DESKTOP-H2UKB99\Jesse Robinson <jbrobinson16@gmail.com>
2020-02-22 12:11:32 -0500

    A level indiactor has been placed in each level. More info more control

---
+++ SpaceRunner/Assets/_Scripts/JesseScripts/JR_Level.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JR_Level : MonoBehaviour
+{
+    //This script is just to assign which level this is - level one, level two, etc. 
+    public int Level;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
