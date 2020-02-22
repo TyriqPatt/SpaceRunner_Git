93bd25796bfed4581901d6442219797e66ed546b
DESKTOP-H2UKB99\Jesse Robinson <jbrobinson16@gmail.com>
2020-02-22 12:23:46 -0500

    Set up script to determine what level will be next then call it and a reset level function.

--- SpaceRunner/Assets/_Scripts/JesseScripts/JR_LevelManager.cs
+++ SpaceRunner/Assets/_Scripts/JesseScripts/JR_LevelManager.cs
@@ -1,18 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class JR_LevelManager : MonoBehaviour
 {
+    //Find the level script to determine what level your in
+    private JR_Level m_Level;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Set the varavle to the script in your scene
+        m_Level = GameObject.FindObjectOfType<JR_Level>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    //Check which scene will be next
+    public void CheckLevel()
+    {
+        //Determine if in level one
+        if(m_Level.Level == 1)
+        {
+            SceneManager.LoadScene("Level_2");
+        }
+        //Determine if in level two
+        else if (m_Level.Level == 2)
+        {
+            SceneManager.LoadScene("Level_3");
+        }
+        //Determine if in level three
+        else if (m_Level.Level == 3)
+        {
+            SceneManager.LoadScene("BossFight");
+        }
+        //Determine if in level four
+        else if (m_Level.Level == 4)
+        {
+            SceneManager.LoadScene("Credits");
+        }
+        //Determine if in level five
+        else if (m_Level.Level == 5)
+        {
+            SceneManager.LoadScene("StartMenu");
+        }
+    }
+
+    public void RestartLevel()
+    {
+        Scene loadedLevel = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(loadedLevel.buildIndex); 
     }
 }
