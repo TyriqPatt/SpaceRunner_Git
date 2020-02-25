26490deaf4feedb2f3d2e96f6c39dc647584df3b
DESKTOP-H2UKB99\Jesse Robinson <jbrobinson16@gmail.com>
2020-02-24 20:06:11 -0500

    Setup win scene to continue through levels. Not yet tested be back later

--- SpaceRunner/Assets/_Scripts/JesseScripts/JR_WinSceneManager.cs
+++ SpaceRunner/Assets/_Scripts/JesseScripts/JR_WinSceneManager.cs
@@ -3,29 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class JR_LoseSceneManager : MonoBehaviour
+public class JR_WinSceneManager : MonoBehaviour
 {
-    public Button m_retryButton;
+    public Button m_nextButton;
     public Button m_quitButton;
     private JR_RestLevel m_resetLevel;
     // Start is called before the first frame update
     void Start()
     {
         m_resetLevel = GameObject.FindObjectOfType<JR_RestLevel>();
-        m_retryButton.onClick.AddListener(m_retryButton_onClick);
+        m_nextButton.onClick.AddListener(m_nextButton_onClick);
         m_quitButton.onClick.AddListener(m_quitButton_onClick);
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void m_retryButton_onClick()
+    void m_nextButton_onClick()
     {
-        m_resetLevel.ResetLevel(); 
+        m_resetLevel.GoToNextLevel(); 
     }
 
     void m_quitButton_onClick()
