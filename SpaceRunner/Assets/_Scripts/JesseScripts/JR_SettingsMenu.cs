9cc33ca79a8c3701b828f46d4746fa7492f83221
DESKTOP-H2UKB99\Jesse Robinson <jbrobinson16@gmail.com>
2020-03-08 18:14:16 -0400

    Added audio mixer and connected slider for volume

---
+++ SpaceRunner/Assets/_Scripts/JesseScripts/JR_SettingsMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class JR_SettingsMenu : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume); 
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+}
